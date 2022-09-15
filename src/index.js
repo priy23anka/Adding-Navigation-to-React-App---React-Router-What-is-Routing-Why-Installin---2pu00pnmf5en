86fd8392703ca10d71cf5601af686a792f8bcf01
priy23anka <105520837+priy23anka@users.noreply.github.com>
2022-09-15 20:43:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
+import { BrowserRouter } from "react-router-dom";
+/*function generateCodeFromObject(obj) {
+  //return a code generated string
+}
+module.exports = generateCodeFromObject;*/
 
-
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>,
+  document.getElementById("root")
+);
+Footer
+© 2022 GitHub, Inc.
